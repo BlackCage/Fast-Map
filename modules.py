3716e3f72aac81924ab5a6314d5acf69483884ad
BlackCage <66648711+BlackCage@users.noreply.github.com>
2022-04-19 03:00:06 +0200

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import json
+import signal
+import socket
+import argparse
+import colorama
+import requests
+from nmap import *
+from colorama import *
+from requests import request
+from requests.exceptions import ConnectionError
